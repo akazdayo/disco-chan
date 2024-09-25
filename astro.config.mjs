4db11204a9256064e3c47f5c3daf1218e0ccdaca
akazdayo <82073147+akazdayo@users.noreply.github.com>
2024-09-25 14:49:13 +0900

    vercelを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [
-		react(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
+  output: "server",
+
+  integrations: [
+      react(),
+      tailwind({
+          applyBaseStyles: false,
+      }),
 	],
+
+  adapter: vercel(),
 });
