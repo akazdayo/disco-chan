0382603d1802e1f1b74af73f0b959beeb994154a
akazdayo <82073147+akazdayo@users.noreply.github.com>
2024-10-30 14:01:27 +0900

    adapterをvercelに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
+	output: "hybrid", // vercelにしよ
 	integrations: [
 		react(),
 		tailwind({
 			applyBaseStyles: false,
 		}),
 	],
-	adapter: node({
-		mode: "standalone",
-	}),
+	adapter: vercel(),
 });
